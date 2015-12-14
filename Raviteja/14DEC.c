93f8a9b55a7e0aae0f9e54ef3231c4bec4567586
sudesh venkatram <sudesh venkatram>
2015-12-14 18:45:03 +0530

    Merge branch 'master' of https://github.com/adityagampa/code-a-day
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    t.array sort

--- Raviteja/14DEC.c
+++ Raviteja/14DEC.c
@@ -1,13 +1,44 @@
 #include <stdio.h>
+#define max 15
 
-void sort(int [],int)
+void display(int a[],int n)
 {
-
+    int i;
+    for(i=0;i<n;i++)
+        printf("%d\t",a[i]);
 }
+
+void sort(int a[],int n)
+{
+    int i,count=0;
+    printf("\nArray before sorting: \n");
+    display(a,n);
+    for(i=0;i<n/2;i++)
+    {
+        if(a[i]==0)
+            count++;
+        if(a[n-1-i]==0)
+            count++;
+    }
+    if(n%2!=0)
+        if(a[n/2]==0)
+            count++;
+    for(i=0;i<n;i++)
+    {
+        if(i<count)
+            a[i]=0;
+        else
+            a[i]=1;
+    }
+    printf("\nArray after sorting: \n");
+    display(a,n);
+}
+
 
 int main()
 {
-    int n,a[max]={0,1,0,1,1,1,0,0,0,1,0};
+    int a[]={0,1,0,1,1,1,0,0,0,1,1};
+    int n=sizeof(a)/sizeof(int);
     sort(a,n);
     return 0;
 }
