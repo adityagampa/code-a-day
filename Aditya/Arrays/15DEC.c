a949a8fa78865c7f704a6d4c34f4b7b504a0e0f8
adityagampa <adityagampa149@gmail.com>
2015-12-15 13:36:36 +0530

    My Dec 15th code -updated..

--- Aditya/Arrays/15DEC.c
+++ Aditya/Arrays/15DEC.c
@@ -6,11 +6,11 @@ void swap(int *a,int num1,int num2)
     a[num2]= a[num1] - a[num2];
     a[num1]= a[num1] - a[num2];
 }
-void * sortedArrayPositionsChange(int *a,int n)
+void sortedArrayPositionsChange(int *a,int n)
 {
     int num1=-1,num2=-1,i=1;
     if(n==0 || n==1)
-        return NULL;
+        return;
     for(i=0; i<n-1; i++)
     {
         if(num1 == -1)
@@ -31,7 +31,7 @@ void * sortedArrayPositionsChange(int *a,int n)
         }
     }
     swap(a,num1,num2);
-    return a;
+    return;
 }
 void display(int *a,int n)
 {
@@ -41,18 +41,10 @@ void display(int *a,int n)
 }
 int main()
 {
-    int n,*a=malloc(n*sizeof(int)),i=0;
-    printf("Enter size: ");
-    scanf("%d",&n);
-    printf("\nEnter elements: ");
-    for(; i<n; i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    a=sortedArrayPositionsChange(a,n);
-    if(a)
-        display(a,n);
-    free(a);
+    int a[]={10,40,30,20,50};
+    int i=0,n=sizeof(a)/sizeof(int);
+    sortedArrayPositionsChange(a,n);
+    display(a,n);
     return 0;
 }
 // worst case time complexity is O(n) where 'n' is the length of the array..
