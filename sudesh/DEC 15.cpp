14815fadb4e068890109c38d4536546b055ad830
sudesh venkatram <sudesh venkatram>
2015-12-15 18:06:11 +0530

    dec15

---
+++ sudesh/DEC 15.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+void sort1(int *a,int n);
+void display(int *a,int n);
+int main()
+{
+	int a[5]={10,40,30,20,50};
+	cout<<"\nbefore sorting";
+	display(a,5);
+	sort1(a,5);
+}
+void sort1(int *a,int n)
+{
+	int i=0,j,temp;
+	for(;i<n;i++)
+	{
+		if(a[i]>a[i+1])
+		{
+			break;
+		}
+	}
+	if(i<n)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				temp=a[j+1];
+				a[j+1]=a[i];
+				a[i]=temp;
+			}
+		}
+	}
+	cout<<"\nafter sorting";
+	display(a,5);	
+}
+void display(int *a,int n)
+{
+	int k=0;
+	for(;k<n;k++)
+	{
+		cout<<"\n"<<a[k];
+	}
+}
+
+
