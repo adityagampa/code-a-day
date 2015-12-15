5fb58076acc00ec6db15276a6575e060bf1fc9d8
vinay kaushik <vinay kaushik>
2015-12-15 12:57:15 +0530

    Dec 15

---
+++ Kaushik/dec15.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void sort_two(int array[],int size_array)
+{
+	int index1=-1;
+	int index2;
+	int i=0;
+	for(;i<size_array;i++)
+	{
+		if(array[i]>array[i+1])
+		{
+			if(index1==-1)
+			index1=i;
+			index2=i+1;	
+		}
+	}
+	//swapping the elements
+	int temp;
+	temp=array[index1];
+	array[index1]=array[index2];
+	array[index2]=temp;
+}
+int main()
+{
+	int array[]={10,20,60,40,50,30};
+	int size_array=sizeof(array)/sizeof(int)-1;
+	sort_two(array,size_array);
+	int j;
+	for(j=0;j<=size_array;j++)
+	{
+		printf(" %d ",array[j]);	
+	}
+}
