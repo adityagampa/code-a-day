d602aca185f9bac9ac771dd16c8e9ebfa773d03a
koushal95 <koushal.ogirala@gmail.com>
2015-12-18 18:03:38 +0530

    updated 16Dec-1 removed allocation of memory for matrix just printed

--- koushal/16Dec-1.c
+++ koushal/16Dec-1.c
@@ -3,8 +3,7 @@
 
 int main(void)
 {
-    int *ptr,size=0,i=0,j=0;
-
+    int size =0,i=0,j=0,var = -1,var1 = 0,var2 = 1;
     printf("\n Enter the size of matrix \n");
     scanf("%d",&size);
     if(size<=0)
@@ -12,41 +11,25 @@ int main(void)
         printf("\n Invalid size \n");
         exit(-1);
     }
-
-        ptr = (int *) malloc(size*size*(sizeof(int)));
-        if(ptr == NULL)
-        {
-            printf("\nCould not create memory \n");
-            exit(-1);
-        }
-
-        for(i=0;i<size;i++)
+     for(i=0;i<size;i++)
         {
             for(j=0;j<size;j++)
             {
                 if(i == j)
                 {
-                    *(ptr + i*size +j) = 0;
+                    printf("%2d ",var1);
                 }
                 else if(i>j)
                 {
-                    *(ptr + i*size +j) = -1;
+                    printf("%2d ",var);
                 }
                 else
                 {
-                        *(ptr + i*size + j) = 1;
+                    printf("%2d ",var2);
                 }
-            }
-        }
-
-        printf("\n The output 2D matrix is: \n");
-        for(i=0;i<size;i++)
-        {
-            for(j=0;j<size;j++)
-            {
-                printf("%2d  ", *(ptr + i*size +j));
             }
             printf("\n");
         }
-        return 0;
+
+    return 0;
 }
